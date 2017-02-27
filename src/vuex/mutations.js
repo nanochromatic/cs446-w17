81f2d247255f2c72b1429f9781b54f6b2e174eb7
Elijah Moreau-Arnott <emoreaua@uwaterloo.ca>
2017-02-27 18:58:56 -0500

    update current colour on card

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -55,6 +55,8 @@ export default {
   playCard (state, card) {
     state.game.deck.find(deckCard => deckCard.color === card.color && deckCard.secondary === card.secondary).location = LOCATION.PLAYED_STACK
     state.game.lastCardPlayed = card
+
+    state.game.lastCardPlayed.color = state.game.currentColour
   },
 
   drawCard (state, playerNumber) {
