89ec7d17087192f8543794d9b0500f45a1227da5
Matt Hayashida <me@mmaatttt.com>
2017-03-27 14:51:01 -0400

    Add some basic animations for cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="card" v-bind:class="['card--'+card.color]">
-  <template v-if="show === true">
-  	<img class="back-frame" :src="'static/cardframe'+card.color+'.png'"/>
-  	<img class="card-icon" :src="'static/card'+card.secondary+card.color+'.png'"/>
-  </template>
-	<img v-else class="back-frame" src="static/cardframeback.png"/>
+  <div class="card" v-bind:class="['card--'+card.location]">
+    <template v-if="show === true">
+    	<img class="back-frame" :src="'static/cardframe'+card.color+'.png'"/>
+    	<img class="card-icon" :src="'static/card'+card.secondary+card.color+'.png'"/>
+    </template>
+  	<img v-else class="back-frame" src="static/cardframeback.png"/>
   </div>
 </template>
 
@@ -34,8 +34,8 @@ export default {
 <style>
 .card {
   position: relative;
+  width: 100%;
   height: 100%;
-  max-width: 20%;
 }
 
 .back-frame {
@@ -51,6 +51,5 @@ export default {
     left: 0;
   height: 100%;
 }
-
 </style>
 
